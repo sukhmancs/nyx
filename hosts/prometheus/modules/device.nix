1bc611576ecf3913a9725f3dd8b5a783224dbb1f
racoot <63659772+sukhmancs@users.noreply.github.com>
2024-05-18 06:34:29 -0400

    tweaking ags

--- hosts/prometheus/modules/device.nix
+++ hosts/prometheus/modules/device.nix
@@ -2,7 +2,7 @@
   config.modules.device = {
     type = "laptop";
     cpu.type = "amd";
-    gpu.type = "nvidia"; # nvidia drivers :b:roke
+    gpu.type = "hybrid-nv"; # "nvidia"; # nvidia drivers :b:roke
     monitors = ["eDP-1" "HDMI-A-1"];
     hasBluetooth = true;
     hasSound = true;
