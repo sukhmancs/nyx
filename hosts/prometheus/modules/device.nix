7aad116c563696cf9b1902d1a86b289ed9afb021
racoot <63659772+sukhmancs@users.noreply.github.com>
2024-05-19 16:53:17 -0400

    commented unnecessary secrets (will setup them later)

--- hosts/prometheus/modules/device.nix
+++ hosts/prometheus/modules/device.nix
@@ -1,12 +1,12 @@
 {
   config.modules.device = {
     type = "laptop";
-    cpu.type = {
+    cpu = {
       type = "amd";
       amd.pstate.enable = false;
       amd.zenpower.enable = false;
     };
-    gpu.type = "hybrid-nv"; # "nvidia"; # nvidia drivers :b:roke
+    gpu.type = null; #"hybrid-nv"; # "nvidia"; # nvidia drivers :b:roke
     monitors = ["eDP-1"];
     hasBluetooth = true;
     hasSound = true;
--- modules/core/common/secrets/default.nix
+++ modules/core/common/secrets/default.nix
@@ -31,9 +31,9 @@ in {
 
   age.secrets = {
     # TODO: system option for declaring host as a potential builder
-    nix-builderKey = mkSecret true {
-      file = "common/nix-builder.age";
-    };
+    # nix-builderKey = mkSecret true {
+    #   file = "common/nix-builder.age";
+    # };
 
     tailscale-client = mkSecret true {
       file = "client/tailscale.age";
@@ -43,152 +43,152 @@ in {
     };
 
     # secrets needed for peers
-    spotify-secret = mkSecret config.modules.system.programs.spotify.enable {
-      file = "client/spotify.age";
-      owner = "notashelf";
-      group = "users";
-      mode = "400";
-    };
+    # spotify-secret = mkSecret config.modules.system.programs.spotify.enable {
+    #   file = "client/spotify.age";
+    #   owner = "notashelf";
+    #   group = "users";
+    #   mode = "400";
+    # };
 
-    wg-client = mkSecret true {
-      file = "client/wg.age";
-      owner = "notashelf";
-      group = "users";
-      mode = "700";
-    };
+    # wg-client = mkSecret true {
+    #   file = "client/wg.age";
+    #   owner = "notashelf";
+    #   group = "users";
+    #   mode = "700";
+    # };
 
-    client-email = mkSecret true {
-      file = "client/email.age";
-      owner = "notashelf";
-      group = "users";
-      mode = "400";
-    };
+    # client-email = mkSecret true {
+    #   file = "client/email.age";
+    #   owner = "notashelf";
+    #   group = "users";
+    #   mode = "400";
+    # };
 
-    # database secrets
-    mongodb-secret = mkSecret cfg.database.mongodb.enable {
-      file = "db/mongodb.age";
-    };
+    # # database secrets
+    # mongodb-secret = mkSecret cfg.database.mongodb.enable {
+    #   file = "db/mongodb.age";
+    # };
 
-    garage-env = mkSecret cfg.database.garage.enable {
-      file = "db/garage.age";
-      mode = "400";
-      owner = "garage";
-      group = "garage";
-    };
+    # garage-env = mkSecret cfg.database.garage.enable {
+    #   file = "db/garage.age";
+    #   mode = "400";
+    #   owner = "garage";
+    #   group = "garage";
+    # };
 
-    # service secrets
-    wg-server = mkSecret cfg.networking.wireguard.enable {
-      file = "service/wg.age";
-    };
+    # # service secrets
+    # wg-server = mkSecret cfg.networking.wireguard.enable {
+    #   file = "service/wg.age";
+    # };
 
-    mkm-web = mkSecret cfg.mkm.enable {
-      file = "service/mkm-web.age";
-      mode = "400";
-    };
+    # mkm-web = mkSecret cfg.mkm.enable {
+    #   file = "service/mkm-web.age";
+    #   mode = "400";
+    # };
 
-    matrix-secret = mkSecret cfg.social.matrix.enable {
-      file = "service/matrix.age";
-      owner = "matrix-synapse";
-      mode = "400";
-    };
+    # matrix-secret = mkSecret cfg.social.matrix.enable {
+    #   file = "service/matrix.age";
+    #   owner = "matrix-synapse";
+    #   mode = "400";
+    # };
 
-    vaultwarden-env = mkSecret cfg.vaultwarden.enable {
-      file = "service/vaultwarden.age";
-      owner = "vaultwarden";
-      mode = "400";
-    };
+    # vaultwarden-env = mkSecret cfg.vaultwarden.enable {
+    #   file = "service/vaultwarden.age";
+    #   owner = "vaultwarden";
+    #   mode = "400";
+    # };
 
-    searx-secretkey = mkSecret cfg.searxng.enable {
-      file = "service/searx.age";
-      mode = "400";
-      owner = "searx";
-      group = "searx";
-    };
+    # searx-secretkey = mkSecret cfg.searxng.enable {
+    #   file = "service/searx.age";
+    #   mode = "400";
+    #   owner = "searx";
+    #   group = "searx";
+    # };
 
-    nextcloud-secret = mkSecret cfg.nextcloud.enable {
-      file = "service/nextcloud.age";
-      mode = "400";
-      owner = "nextcloud";
-      group = "nextcloud";
-    };
+    # nextcloud-secret = mkSecret cfg.nextcloud.enable {
+    #   file = "service/nextcloud.age";
+    #   mode = "400";
+    #   owner = "nextcloud";
+    #   group = "nextcloud";
+    # };
 
-    attic-env = mkSecret cfg.bincache.atticd.enable {
-      file = "service/attic.age";
-      mode = "400";
-      owner = "atticd";
-      group = "atticd";
-    };
+    # attic-env = mkSecret cfg.bincache.atticd.enable {
+    #   file = "service/attic.age";
+    #   mode = "400";
+    #   owner = "atticd";
+    #   group = "atticd";
+    # };
 
-    harmonia-privateKey = mkSecret cfg.bincache.harmonia.enable {
-      file = "service/harmonia.age";
-      mode = "770";
-      owner = "harmonia";
-      group = "harmonia";
-    };
+    # harmonia-privateKey = mkSecret cfg.bincache.harmonia.enable {
+    #   file = "service/harmonia.age";
+    #   mode = "770";
+    #   owner = "harmonia";
+    #   group = "harmonia";
+    # };
 
-    forgejo-runner-token = mkSecret cfg.forgejo.enable {
-      file = "service/forgejo-runner-token.age";
-      mode = "400";
-      owner = "gitea-runner";
-      group = "gitea-runner";
-    };
+    # forgejo-runner-token = mkSecret cfg.forgejo.enable {
+    #   file = "service/forgejo-runner-token.age";
+    #   mode = "400";
+    #   owner = "gitea-runner";
+    #   group = "gitea-runner";
+    # };
 
-    forgejo-runner-config = mkSecret cfg.forgejo.enable {
-      file = "service/forgejo-runner-config.age";
-      mode = "400";
-      owner = "gitea-runner";
-      group = "gitea-runner";
-    };
+    # forgejo-runner-config = mkSecret cfg.forgejo.enable {
+    #   file = "service/forgejo-runner-config.age";
+    #   mode = "400";
+    #   owner = "gitea-runner";
+    #   group = "gitea-runner";
+    # };
 
-    headscale-derp = mkSecret cfg.networking.headscale.enable {
-      file = "service/headscale-derp.age";
-      mode = "400";
-      owner = "headscale";
-      group = "headscale";
-    };
+    # headscale-derp = mkSecret cfg.networking.headscale.enable {
+    #   file = "service/headscale-derp.age";
+    #   mode = "400";
+    #   owner = "headscale";
+    #   group = "headscale";
+    # };
 
-    headscale-noise = mkSecret cfg.networking.headscale.enable {
-      file = "service/headscale-noise.age";
-      mode = "400";
-      owner = "headscale";
-      group = "headscale";
-    };
+    # headscale-noise = mkSecret cfg.networking.headscale.enable {
+    #   file = "service/headscale-noise.age";
+    #   mode = "400";
+    #   owner = "headscale";
+    #   group = "headscale";
+    # };
 
-    # mailserver secrets
-    mailserver-secret = mkSecret cfg.mailserver.enable {
-      file = "mailserver/postmaster.age";
-      mode = "400";
-    };
+    # # mailserver secrets
+    # mailserver-secret = mkSecret cfg.mailserver.enable {
+    #   file = "mailserver/postmaster.age";
+    #   mode = "400";
+    # };
 
-    mailserver-forgejo-secret = mkSecret cfg.forgejo.enable {
-      file = "mailserver/forgejo.age";
-      owner = "forgejo";
-      group = "forgejo";
-      mode = "400";
-    };
+    # mailserver-forgejo-secret = mkSecret cfg.forgejo.enable {
+    #   file = "mailserver/forgejo.age";
+    #   owner = "forgejo";
+    #   group = "forgejo";
+    #   mode = "400";
+    # };
 
-    mailserver-vaultwarden-secret = mkSecret cfg.vaultwarden.enable {
-      file = "mailserver/vaultwarden.age";
-      owner = "vaultwarden";
-      mode = "400";
-    };
+    # mailserver-vaultwarden-secret = mkSecret cfg.vaultwarden.enable {
+    #   file = "mailserver/vaultwarden.age";
+    #   owner = "vaultwarden";
+    #   mode = "400";
+    # };
 
-    mailserver-cloud-secret = mkSecret cfg.nextcloud.enable {
-      file = "mailserver/cloud.age";
-      owner = "nextcloud";
-      mode = "400";
-    };
+    # mailserver-cloud-secret = mkSecret cfg.nextcloud.enable {
+    #   file = "mailserver/cloud.age";
+    #   owner = "nextcloud";
+    #   mode = "400";
+    # };
 
-    mailserver-matrix-secret = mkSecret cfg.social.matrix.enable {
-      file = "mailserver/matrix.age";
-      owner = "matrix-synapse";
-      mode = "400";
-    };
+    # mailserver-matrix-secret = mkSecret cfg.social.matrix.enable {
+    #   file = "mailserver/matrix.age";
+    #   owner = "matrix-synapse";
+    #   mode = "400";
+    # };
 
-    mailserver-noreply-secret = mkSecret cfg.social.mastodon.enable {
-      file = "mailserver/noreply.age";
-      owner = "mastodon";
-      mode = "400";
-    };
+    # mailserver-noreply-secret = mkSecret cfg.social.mastodon.enable {
+    #   file = "mailserver/noreply.age";
+    #   owner = "mastodon";
+    #   mode = "400";
+    # };
   };
 }
