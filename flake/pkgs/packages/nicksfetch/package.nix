1019331c2bf2df30663cc470558bb4ab4c1f95b1
racoot <63659772+sukhmancs@users.noreply.github.com>
2024-05-21 00:59:41 -0400

    added more nvfetcher toml files

--- flake/pkgs/packages/nicksfetch/package.nix
+++ flake/pkgs/packages/nicksfetch/package.nix
@@ -1,51 +1,54 @@
 {
   lib,
   stdenvNoCC,
+  callPackages,
   fetchFromGitHub,
   bash,
   makeWrapper,
   pciutils,
   x11Support ? true,
   ueberzug,
-}:
-stdenvNoCC.mkDerivation {
-  pname = "nicksfetch";
-  version = "unstable-2021-12-10";
+}: let
+  nv = (callPackages ./generated.nix {}).nicksfetch;
+in
+  stdenvNoCC.mkDerivation {
+    inherit (nv) pname src;
+    version = nv.date;
 
-  src = fetchFromGitHub {
-    owner = "dylanaraps";
-    repo = "neofetch";
-    rev = "ccd5d9f52609bbdcd5d8fa78c4fdb0f12954125f";
-    sha256 = "sha256-9MoX6ykqvd2iB0VrZCfhSyhtztMpBTukeKejfAWYW1w=";
-  };
+    #    src = fetchFromGitHub {
+    #      owner = "dylanaraps";
+    #      repo = "neofetch";
+    #      rev = "ccd5d9f52609bbdcd5d8fa78c4fdb0f12954125f";
+    #      sha256 = "sha256-9MoX6ykqvd2iB0VrZCfhSyhtztMpBTukeKejfAWYW1w=";
+    #    };
 
-  patches = [./0001-nicksfetch.patch];
+    patches = [./0001-nicksfetch.patch];
 
-  outputs = ["out" "man"];
+    outputs = ["out" "man"];
 
-  strictDeps = true;
-  buildInputs = [bash];
-  nativeBuildInputs = [makeWrapper];
-  postPatch = ''
-    patchShebangs --host neofetch
-  '';
+    strictDeps = true;
+    buildInputs = [bash];
+    nativeBuildInputs = [makeWrapper];
+    postPatch = ''
+      patchShebangs --host neofetch
+    '';
 
-  postInstall = ''
-    wrapProgram $out/bin/neofetch \
-      --prefix PATH : ${lib.makeBinPath ([pciutils] ++ lib.optional x11Support ueberzug)}
-  '';
+    postInstall = ''
+      wrapProgram $out/bin/neofetch \
+        --prefix PATH : ${lib.makeBinPath ([pciutils] ++ lib.optional x11Support ueberzug)}
+    '';
 
-  makeFlags = [
-    "PREFIX=${placeholder "out"}"
-    "SYSCONFDIR=${placeholder "out"}/etc"
-  ];
+    makeFlags = [
+      "PREFIX=${placeholder "out"}"
+      "SYSCONFDIR=${placeholder "out"}/etc"
+    ];
 
-  meta = {
-    description = "A fast, highly customizable system info script";
-    homepage = "https://github.com/dylanaraps/neofetch";
-    license = lib.licenses.mit;
-    platforms = lib.platforms.all;
-    maintainers = with lib.maintainers; [alibabzo konimex notashelf];
-    mainProgram = "neofetch";
-  };
-}
+    meta = {
+      description = "A fast, highly customizable system info script";
+      homepage = "https://github.com/dylanaraps/neofetch";
+      license = lib.licenses.mit;
+      platforms = lib.platforms.all;
+      maintainers = with lib.maintainers; [alibabzo konimex notashelf];
+      mainProgram = "neofetch";
+    };
+  }
