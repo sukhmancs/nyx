3ec17436d9a3cb6fa756419766f153912fe03358
racoot <63659772+sukhmancs@users.noreply.github.com>
2024-06-01 14:10:41 -0400

    added some tweaks

--- hosts/hermes/wireguard.nix
+++ hosts/hermes/wireguard.nix
@@ -19,7 +19,7 @@
           {
             allowedIPs = ["10.255.255.0/24" "2a01:4f9:c010:2cf9:f::/80"];
             endpoint = "128.140.91.216:51820";
-            publicKey = "v3ol3QsgLPudVEtbETByQ0ABAOrJE2WcFfQ/PQAD8FM=";
+            publicKey = "hT+c+1AHCem2jcYpk3cq3XqTVRHt/sxJXd6oi+fUYzM=";
             persistentKeepalive = 30;
           }
         ];
