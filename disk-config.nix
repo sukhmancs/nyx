6391c94a64524aac466adbb21257dceb824c093f
racoot <63659772+sukhmancs@users.noreply.github.com>
2024-06-06 20:47:08 -0400

    added disko configuration for helios host on nix-anywhere

---
+++ disk-config.nix
@@ -0,0 +1,44 @@
+{
+  disko.devices = {
+    disk = {
+      vdb = {
+        type = "disk";
+        device = "/dev/sda";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              priority = 1;
+              name = "ESP";
+              start = "1M";
+              end = "128M";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+              };
+            };
+            root = {
+              end = "-2G";
+              content = {
+                type = "btrfs";
+                extraArgs = ["-f"]; # Override existing partition
+                mountpoint = "/";
+                mountOptions = ["compress=zstd" "noatime"];
+              };
+            };
+            plainSwap = {
+              size = "100%";
+              content = {
+                type = "swap";
+                discardPolicy = "both";
+                resumeDevice = true; # resume from hiberation from this device
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
