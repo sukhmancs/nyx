d053d73862292b201fb7652be9a3f57ff5b87f12
racoot <63659772+sukhmancs@users.noreply.github.com>
2024-06-15 16:23:08 -0400

    added adguardhome dns server

--- modules/core/roles/server/system/services/adguard.nix
+++ modules/core/roles/server/system/services/adguard.nix
@@ -112,13 +112,14 @@ in {
 
       nginx.virtualHosts."${domain}" =
         {
+          useACMEHost = "xilain.dev";
           locations."/" = {
             proxyPass = "http://127.0.0.1:${toString port}";
-            extraConfig = ''
-              proxy_set_header Host $host;
-              proxy_set_header X-Real-IP $remote_addr;
-              proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-            '';
+            # extraConfig = ''
+            #   proxy_set_header Host $host;
+            #   proxy_set_header X-Real-IP $remote_addr;
+            #   proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+            # '';
           };
 
           #   quic = true;
