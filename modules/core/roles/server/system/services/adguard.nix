e7efe60beed47771cb865ff4c1f649e745d64040
racoot <63659772+sukhmancs@users.noreply.github.com>
2024-06-15 15:32:21 -0400

    added adguardhome dns server

--- modules/core/roles/server/system/services/adguard.nix
+++ modules/core/roles/server/system/services/adguard.nix
@@ -63,13 +63,14 @@ in {
         openFirewall = true;
         settings = {
           http.address = "${host}:${toString port}";
-          #   schema_version = 20;
+          schema_version = 20;
           dns = {
             ratelimit = 20; # DDoS protection
             refuse_any = true; # Request of type ANY will be refused
             enable_dnssec = true; # DNSSEC validation
             theme = "dark"; # Dark theme
             bind_hosts = ["127.0.0.1"];
+            port = 53;
             anonymize_client_ip = true;
             bootstrap_dns = [
               "9.9.9.10"
@@ -119,7 +120,7 @@ in {
             '';
           };
 
-          quic = true;
+          #   quic = true;
         }
         // lib.sslTemplate;
     };
