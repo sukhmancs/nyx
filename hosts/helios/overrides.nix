6dc195accfba1b5abe12358b012f91c9c6e4e9ba
racoot <63659772+sukhmancs@users.noreply.github.com>
2024-06-16 16:48:57 -0400

    added authelia

--- hosts/helios/overrides.nix
+++ hosts/helios/overrides.nix
@@ -1,8 +1,9 @@
 {lib, ...}: {
   config = {
     # services.qemuGuest.enable = lib.mkForce true;
-
+    services.resolved.enable = lib.mkForce false; # use adguardhome for dns
     networking = {
+      networkManager.dns = lib.mkForce "127.0.0.1"; # use adguardhome for dns
       defaultGateway = "102.209.85.225";
       defaultGateway6 = {
         address = "";
