c6e42d902a3cbc537f873109f7eedd374bec4028
racoot <63659772+sukhmancs@users.noreply.github.com>
2024-06-17 11:54:49 -0400

    added authelia

--- modules/core/roles/server/system/services/lldap.nix
+++ modules/core/roles/server/system/services/lldap.nix
@@ -7,41 +7,40 @@
 
   inherit (cfg.ldap.settings) port host;
 in {
-  config =
-    mkIf cfg.ldap.enable
-    || cfg.authelia.enable {
-      modules.system.services = {
-        nginx.enable = true;
-      };
-
-      services = {
-        lldap = {
-          enable = true;
-          settings = {
-            http_url = "https://${domain}";
-            ldap_base_dn = "dc=xilain,dc=xyz";
-            key_file = secrets.lldap_private_key.path;
-          };
-          environment = {
-            LLDAP_JWT_SECRET_FILE = secrets.lldap_jwt_secret.path;
-            LLDAP_LDAP_USER_PASS_FILE = secrets.lldap_user_pass.path;
-          };
-        };
-
-        nginx.virtualHosts.${domain} =
-          {
-            locations."/" = {
-              # TODO: the port is not customizable in the upstream service, PR nixpkgs
-              proxyPass = "http://${host}:${cfg.ldap.settings.port}";
-              proxyWebsockets = true;
-              extraConfig = "proxy_pass_header Authorization;";
-            };
-
-            quic = true;
-          }
-          // lib.sslTemplate;
-      };
-
-      systemd.services.lldap.serviceConfig.SupplementaryGroups = ["lldap-secrets"];
+  config = mkIf (cfg.ldap.enable
+    || cfg.authelia.enable) {
+    modules.system.services = {
+      nginx.enable = true;
     };
+
+    services = {
+      lldap = {
+        enable = true;
+        settings = {
+          http_url = "https://${domain}";
+          ldap_base_dn = "dc=xilain,dc=xyz";
+          key_file = secrets.lldap_private_key.path;
+        };
+        environment = {
+          LLDAP_JWT_SECRET_FILE = secrets.lldap_jwt_secret.path;
+          LLDAP_LDAP_USER_PASS_FILE = secrets.lldap_user_pass.path;
+        };
+      };
+
+      nginx.virtualHosts.${domain} =
+        {
+          locations."/" = {
+            # TODO: the port is not customizable in the upstream service, PR nixpkgs
+            proxyPass = "http://${host}:${cfg.ldap.settings.port}";
+            proxyWebsockets = true;
+            extraConfig = "proxy_pass_header Authorization;";
+          };
+
+          quic = true;
+        }
+        // lib.sslTemplate;
+    };
+
+    systemd.services.lldap.serviceConfig.SupplementaryGroups = ["lldap-secrets"];
+  };
 }
