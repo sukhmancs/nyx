edcc4f8fcecbae4379dbe457e7be044e2b36de71
racoot <63659772+sukhmancs@users.noreply.github.com>
2024-06-22 01:46:47 -0400

    setup attic and harmonio

--- homes/notashelf/services/wayland/waybar/presets/catppuccin-mocha/config.nix
+++ homes/notashelf/services/wayland/waybar/presets/catppuccin-mocha/config.nix
@@ -128,7 +128,6 @@ in {
       orientation = "inherit";
       modules = [
         "custom/cpu-icon"
-        "custom/cputemp"
         "cpu"
       ];
     };
@@ -171,68 +170,6 @@ in {
       format = "󰇘";
       tooltip = true;
       tooltip-format = "Screen sharing is active";
-    };
-
-    "custom/cputemp" = {
-      format = "{}";
-      interval = 10;
-      "return-type" = "json";
-      exec = let
-        data = "$(sensors asus-isa-0000 | sed 's/+//g')";
-        package = "$(echo ${data} | awk -e '/Package/ {print $4}')";
-        coretemp = "$(echo ${data} | awk -e '/Core/ {print $3}')";
-
-        # tooltip = "<b>Core Temp: $package </b>\n";
-        tempint = "$(echo ${package} | cut -d. -f1)";
-        temp = "<b>${tempint}󰔄</b>";
-      in
-        pkgs.writeShellScript "cputemp-waybar" ''
-          #!/bin/sh
-
-          check() {
-            command -v "$1" 1>/dev/null
-          }
-
-          check sensors || exit
-
-          # data="$(sensors coretemp-isa-0000 | sed 's/+//g')"
-          # package="$(echo "$data" | awk -e '/Package/ {print $4}')"
-          # coretemp="$(echo "$data" | awk -e '/Core/ {print $3}')"
-
-          tooltip="<b>Core Temp: ${package} </b>\n"
-
-          # "format-icons" : [ "", "", "", "", "" ] ,
-          # tempint="$(echo ${package} | cut -d. -f1)"
-          # temp="<b>${tempint}󰔄</b>"
-          icon=""
-          class="cool"
-          [ "${tempint}" -gt 50 ] && {
-            icon=""
-            class="normal"
-          }
-          [ "${tempint}" -gt 70 ] && {
-            icon=" "
-            class="warm"
-          }
-          [ "${tempint}" -gt 85 ] && {
-            icon=" "
-            class="warn"
-          }
-          [ "${tempint}" -gt 95 ] && {
-            icon=" "
-            class="critical"
-          }
-
-          j=0
-          for i in ${coretemp}; do
-            tooltip+="Core $j: $i\n"
-            ((j = j + 1))
-          done
-          tooltip="${tooltip::-2}"
-          cat <<EOF
-          {"text":"$temp","tooltip":"$tooltip", "class": "$class"}
-          EOF
-        '';
     };
 
     "custom/todo" = {
