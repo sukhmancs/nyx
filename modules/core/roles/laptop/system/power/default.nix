c7de63b97f506e99f94f28c884ffdd934640d5e0
racoot <63659772+sukhmancs@users.noreply.github.com>
2024-06-24 02:03:55 -0400

    setup attic and harmonio

--- modules/core/roles/laptop/system/power/default.nix
+++ modules/core/roles/laptop/system/power/default.nix
@@ -24,7 +24,7 @@ in {
       acpid.enable = true;
 
       # allows changing system behavior based upon user-selected power profiles
-      power-profiles-daemon.enable = false;
+      power-profiles-daemon.enable = true;
 
       # temperature target on battery
       undervolt = {
