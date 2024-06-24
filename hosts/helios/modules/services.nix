a3a4bfa6daca2961afc6fc531f458c26709602d0
racoot <63659772+sukhmancs@users.noreply.github.com>
2024-06-24 17:50:42 -0400

    setup attic and harmonio

--- hosts/helios/modules/services.nix
+++ hosts/helios/modules/services.nix
@@ -1,16 +1,16 @@
 {
   config.modules.system.services = {
-    nextcloud.enable = true;
-    mailserver.enable = true;
+    nextcloud.enable = false; #Change me
+    mailserver.enable = false; #Change me
     vaultwarden.enable = true;
-    forgejo.enable = true;
+    forgejo.enable = false; #Change me
     searxng.enable = true;
     reposilite.enable = false;
-    invidious.enable = true;
-    suwayomi-server.enable = true;
-    adguard.enable = true;
-    netdata.enable = true;
-    authelia.enable = true;
+    invidious.enable = false; #Change me
+    suwayomi-server.enable = false; #Change me
+    adguard.enable = false; #Change me
+    netdata.enable = false; #Change me
+    authelia.enable = false; #Change me
     jitsi-meet.enable = false; #FIXME: Remove jitsi-meet as i was unable to find a way for jitsi-meet to listen on different port other than 443. I am already serving my own website on that port.And that is where nginx is listening
 
     homelab = {
@@ -18,12 +18,12 @@
     };
 
     social = {
-      mastodon.enable = true;
-      matrix.enable = true;
+      mastodon.enable = false; #Change me
+      matrix.enable = false; #Change me
     };
 
     bincache = {
-      harmonia.enable = true;
+      harmonia.enable = false; #Change me
     };
 
     networking = {
