ce30c93ab341cc6e63d80cd686354d20799c0a1e
racoot <63659772+sukhmancs@users.noreply.github.com>
2024-07-03 05:53:09 -0400

    added some tweaks

--- flake/pkgs/default.nix
+++ flake/pkgs/default.nix
@@ -4,11 +4,22 @@
     pkgs,
     lib,
     ...
-  }: {
+  }: let
+    # Dynamically import all Nix files from the directory
+    importPackages = directory:
+      lib.mapAttrs' (
+        name: type:
+          pkgs.callPackage (directory + "/" + name + "/package.nix") {}
+      ) (builtins.readDir directory);
+
+    # Import all packages from the packages directory
+    packages = importPackages ./packages;
+  in {
     overlayAttrs = config.packages;
-    packages = lib.packagesFromDirectoryRecursive {
-      inherit (pkgs) callPackage;
-      directory = ./packages;
-    };
+    packages = packages;
+    # packages = lib.packagesFromDirectoryRecursive {
+    #   inherit (pkgs) callPackage;
+    #   directory = ./packages;
+    # };
   };
 }
--- flake/pkgs/packages/anime4k/package.nix
+++ flake/pkgs/packages/anime4k/package.nix
@@ -3,14 +3,20 @@
   stdenvNoCC,
   fetchzip,
 }:
-stdenvNoCC.mkDerivation (finalAttrs: {
+stdenvNoCC.mkDerivation rec {
   pname = "anime4k";
   version = "4.0.1";
 
-  src = fetchzip {
-    url = "https://github.com/bloc97/Anime4K/releases/download/v${finalAttrs.version}/Anime4K_v4.0.zip";
-    stripRoot = false;
-    sha256 = "18x5q7zvkf5l0b2phh70ky6m99fx1pi6mhza4041b5hml7w987pl";
+  # src = fetchzip {
+  #   url = "https://github.com/bloc97/Anime4K/releases/download/v${finalAttrs.version}/Anime4K_v4.0.zip";
+  #   stripRoot = false;
+  #   sha256 = "18x5q7zvkf5l0b2phh70ky6m99fx1pi6mhza4041b5hml7w987pl";
+  # };
+  src = fetchFromGitHub {
+    owner = "bloc97";
+    repo = "Anime4K";
+    rev = "v${version}";
+    sha256 = "1nxh8h00h0smrnq5s6a4zvhxgkz2gaxaq2kggxdkicxi6z6w94wr";
   };
 
   installPhase = ''
@@ -18,9 +24,10 @@ stdenvNoCC.mkDerivation (finalAttrs: {
     cp *.glsl $out
   '';
 
-  meta = {
+  meta = with lib; {
     description = "A High-Quality Real Time Upscaler for Anime Video";
     homepage = "https://github.com/bloc97/Anime4K";
-    license = lib.licenses.mit;
+    license = licenses.mit;
+    maintainers = with maintainers; [bloc97];
   };
-})
+}
