67a4ba6fb23add660fa5778f1f4c5c83d1318f21
GitHub <noreply@github.com>
2024-07-03 14:08:39 +0000

    Bump flake/pkgs/packages/nicksfetch/nvfetcher.toml
    Co-authored-by: sukhmancs <sukhmancs@users.noreply.github.com>

--- flake/pkgs/packages/nicksfetch/generated.nix
+++ flake/pkgs/packages/nicksfetch/generated.nix
@@ -9,7 +9,7 @@
       repo = "neofetch";
       rev = "ccd5d9f52609bbdcd5d8fa78c4fdb0f12954125f";
       fetchSubmodules = false;
-      sha256 = "sha256-9MoX6ykqvd2iB0VrZCfhSyhtztMpBTukeKejfAWYW1w=";
+      sha256 = "sha256-hTWURmH+Z5q9eUJZOf6NAuu/n40COyrEeOOKk3qAIVo=";
     };
     date = "2021-12-10";
   };
