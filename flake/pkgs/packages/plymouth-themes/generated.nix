994bed0f0cf481cae1283aa1b5d832c4f92100f8
GitHub <noreply@github.com>
2024-07-03 13:44:17 +0000

    Bump flake/pkgs/packages/plymouth-themes/nvfetcher.toml
    Co-authored-by: sukhmancs <sukhmancs@users.noreply.github.com>

--- flake/pkgs/packages/plymouth-themes/generated.nix
+++ flake/pkgs/packages/plymouth-themes/generated.nix
@@ -9,7 +9,7 @@
       repo = "plymouth-themes";
       rev = "5d8817458d764bff4ff9daae94cf1bbaabf16ede";
       fetchSubmodules = false;
-      sha256 = "sha256-e3lRgIBzDkKcWEp5yyRCzQJM6yyTjYC5XmNUZZroDuw=";
+      sha256 = "sha256-3Au6EH62MQMuvyDDarz2ahaJe3pnfNzUwyYo80WWKw0=";
     };
     date = "2023-08-22";
   };
