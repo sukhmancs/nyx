02f2dcc96c8b68967efb3cf75d7895b1215d90d9
racoot <63659772+sukhmancs@users.noreply.github.com>
2024-07-06 20:22:45 -0400

    added some tweaks

--- hosts/milkyway/modules/home.nix
+++ hosts/milkyway/modules/home.nix
@@ -1,22 +1,22 @@
 {
-  home-manager.users.xi = {
-    wayland.windowManager.hyprland.enable = true;
-    services.dunst.enable = true;
-    services.gammastep.enable = true;
-    services.nextcloud-client.enable = true;
-    programs = {
-      rofi.enable = true;
-      firefox.enable = true;
-      thunderbird.enable = true;
-      vscode.enable = true;
-      mangohud.enable = true;
-      swaylock.enable = true;
-      mpv.enable = true;
-    };
-    gtk.enable = true;
-  };
-
   config = {
+    home-manager.users.xi = {
+      wayland.windowManager.hyprland.enable = true;
+      services.dunst.enable = true;
+      services.gammastep.enable = true;
+      services.nextcloud-client.enable = true;
+      programs = {
+        rofi.enable = true;
+        firefox.enable = true;
+        thunderbird.enable = true;
+        vscode.enable = true;
+        mangohud.enable = true;
+        swaylock.enable = true;
+        mpv.enable = true;
+      };
+      gtk.enable = true;
+    };
+
     modules.home.programs.clipboard.enable = true;
     modules.home.programs.hyprpaper.enable = true;
     modules.home.programs.spotify.enable = true;
